9ba59edc84da59a938630b4112734f322594307a
H1mar1 <1226Himari@au.com>
2025-12-15 18:44:39 +0900

    敵の落下の追加

--- ShootingGame/Assets/Scripts/enemyManager.cs
+++ ShootingGame/Assets/Scripts/enemyManager.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class enemyManager : MonoBehaviour
@@ -7,13 +8,17 @@ public class enemyManager : MonoBehaviour
     private GameObject enemyPrefab;
     [SerializeField,Header("敵のスポーン間隔")]
     private float spwenInterval;
+    [SerializeField, Header("敵の落下スピード")]
+    private float enemyDownSpeed;
 
     private Vector2 spawnRangeX = new Vector2(-2.48f, 2.48f);
     private float spawnY = 5.6f;
 
-    private void Start()
+    private async void Start()
     {
         enemySpan().Forget();
+        await UniTask.Delay(1000);
+        enemyDown();
     }
 
     async UniTask enemySpan()
@@ -30,6 +35,29 @@ public class enemyManager : MonoBehaviour
         float x = Random.Range(spawnRangeX.x, spawnRangeX.y);
         Vector3 spawnPosition = new Vector3(x, spawnY, 0.0f);
 
+        Debug.Log("あはははは");
+
         Instantiate(enemyPrefab, spawnPosition , Quaternion.identity);
     }
+
+    private async void enemyDown()
+    {
+        while (true)
+        {
+            // Enemy タグが付いたオブジェクトを全て取得
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+            foreach (GameObject enemy in enemies)
+            {
+                if (enemy == null) continue; // null のときだけスキップ
+
+                enemy.transform.position +=
+                    Vector3.down * enemyDownSpeed * Time.deltaTime;
+            }
+
+            //1フレーム
+            await UniTask.Yield();
+        }
+    }
+
 }
